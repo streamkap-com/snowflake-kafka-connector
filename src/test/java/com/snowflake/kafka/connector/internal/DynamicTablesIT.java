93cf81b58bcfdbdc282594c1649e7e1231f58539
Alexandru Cristu <cristualexandru@gmail.com>
2023-05-31 08:49:21 +0300

    STR-951: create dynamic table via jdbc

--- src/test/java/com/snowflake/kafka/connector/internal/DynamicTablesIT.java
+++ src/test/java/com/snowflake/kafka/connector/internal/DynamicTablesIT.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class DynamicTablesIT {
@@ -14,6 +15,7 @@ public class DynamicTablesIT {
       .build();
 
   @Test
+  @Ignore("draft uses manually created table")
   public void testGetDynamicTableMetadata() throws Exception {
     DatabaseMetaData dm = conn.getDatabaseMetadata();
 
@@ -28,6 +30,34 @@ public class DynamicTablesIT {
     printResultSet(dm.getCatalogs());
     printResultSet(dm.getSchemas());
     printResultSet(dm.getColumns("%", "%", "%", null));
+  }
+
+  @Test
+  // @Ignore("draft uses manually created table")
+  public void testCreateDynamicTable() throws Exception {
+    DatabaseMetaData dm = conn.getDatabaseMetadata();
+
+    String stmStr = ""
+      + " CREATE OR REPLACE DYNAMIC TABLE TSTTBL_junit_V"
+      + "   lag = '1 seconds'"
+      + "   warehouse = STREAMKAP_WH"
+      + " AS SELECT * EXCLUDE(dedupe_id)"
+      + "         FROM ("
+      + "         SELECT *,"
+      + "             ROW_NUMBER() OVER "
+      + "                 (PARTITION BY id "
+      + "                 ORDER BY CREATED_AT DESC) AS dedupe_id"
+      + "             FROM TSTTBL)"
+      + "         WHERE dedupe_id = 1        -- Latest record"
+      + "             AND __deleted = 'false';  -- Excluding deleted record"
+    ;
+    System.out.println("## creating TSTTBL_junit_V ###########################");
+    PreparedStatement stm = dm.getConnection().prepareStatement(stmStr);
+    stm.execute();
+
+    System.out.println("## TSTTBL_junit_V columns ###########################");
+    stm = dm.getConnection().prepareStatement("DESCRIBE TABLE TSTTBL_junit_V");
+    printResultSet(stm.executeQuery());
   }
 
   private void printResultSet(ResultSet rs) throws SQLException {
