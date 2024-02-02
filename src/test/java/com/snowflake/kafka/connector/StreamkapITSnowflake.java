ae72fc2f4e02ecf331ba02f297ee9fa3f7fa62e8
EduardHantig <h.eduardgabor@gmail.com>
2024-02-02 14:11:44 +0200

    Upgrade Snowflake to 2.1.2 - trial fix StreamkapITs

--- src/test/java/com/snowflake/kafka/connector/StreamkapITSnowflake.java
+++ src/test/java/com/snowflake/kafka/connector/StreamkapITSnowflake.java
@@ -5,15 +5,11 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.transforms.ReplaceField;
 import org.junit.jupiter.api.Test;
 
-import com.snowflake.client.jdbc.SnowflakeDriver;
-import com.snowflake.kafka.connector.internal.InternalUtils;
-import com.snowflake.kafka.connector.internal.SnowflakeURL;
 import com.snowflake.kafka.connector.internal.TestUtils;
 import com.streamkap.common.test.sink.StreamkapSinkITBase;
 
@@ -29,7 +25,16 @@ public class StreamkapITSnowflake extends StreamkapSinkITBase<SnowflakeSinkTask>
                 "account:_account,all:_all,alter:_alter,and:_and,any:_any,as:_as,between:_between,by:_by,case:_case,cast:_cast,check:_check,column:_column,connect:_connect,connection:_connection,constraint:_constraint,create:_create,cross:_cross,current:_current,current_date:_current_date,current_time:_current_time,current_timestamp:_current_timestamp,current_user:_current_user,database:_database,delete:_delete,distinct:_distinct,drop:_drop,else:_else,exists:_exists,false:_false,following:_following,for:_for,from:_from,full:_full,grant:_grant,group:_group,gscluster:_gscluster,having:_having,ilike:_ilike,in:_in,increment:_increment,inner:_inner,insert:_insert,intersect:_intersect,into:_into,is:_is,issue:_issue,join:_join,lateral:_lateral,left:_left,like:_like,localtime:_localtime,localtimestamp:_localtimestamp,minus:_minus,natural:_natural,not:_not,null:_null,of:_of,on:_on,or:_or,order:_order,organization:_organization,qualify:_qualify,regexp:_regexp,revoke:_revoke,right:_right,rlike:_rlike,row:_row,rows:_rows,sample:_sample,schema:_schema,select:_select,set:_set,some:_some,start:_start,table:_table,tablesample:_tablesample,then:_then,to:_to,trigger:_trigger,true:_true,try_cast:_try_cast,union:_union,unique:_unique,update:_update,using:_using,values:_values,view:_view,when:_when,whenever:_whenever,where:_where,with:_with");
         renameAmbigiousFields.configure(config);
 
-        super.init(TestUtils.generateConnectionToSnowflakeWithEncryptedKey());
+        super.init(new com.streamkap.common.test.TestUtils() {
+            @Override
+            protected Connection createCon() {
+                try {
+                return TestUtils.generateConnectionToSnowflakeWithEncryptedKey();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        });
     }
 
     public Map<String, String> getConf() {
@@ -37,18 +42,29 @@ public class StreamkapITSnowflake extends StreamkapSinkITBase<SnowflakeSinkTask>
         config.put("behavior.on.null.values", "IGNORE");
         config.put("connector.class", "com.snowflake.kafka.connector.SnowflakeSinkConnector");
         config.put("name", "destination_65019e4b368473fede109f76");
-        config.put("snowflake.database.name", confFromJson.get(Utils.SF_DATABASE));
+//        config.put("snowflake.database.name", confFromJson.get(Utils.SF_DATABASE));
+        config.put(Utils.SF_DATABASE, "JUNIT");
         config.put("snowflake.enable.schematization", "true");
         config.put("snowflake.ingestion.method", "SNOWPIPE_STREAMING");
-        config.put("snowflake.private.key", confFromJson.get(Utils.SF_PRIVATE_KEY));
-        config.put("snowflake.private.key.passphrase", confFromJson.get(Utils.PRIVATE_KEY_PASSPHRASE));
-        config.put("snowflake.role.name", confFromJson.get(Utils.SF_ROLE));
-        config.put("snowflake.schema.name", confFromJson.get(Utils.SF_SCHEMA));
+
+        config.put(Utils.SF_PRIVATE_KEY, System.getenv("SNOWFLAKE_KEY"));
+//        config.put("snowflake.private.key", confFromJson.get(Utils.SF_PRIVATE_KEY));
+
+        config.put(Utils.PRIVATE_KEY_PASSPHRASE, System.getenv("SNOWFLAKE_KEY_PASS"));
+//        config.put("snowflake.private.key.passphrase", confFromJson.get(Utils.PRIVATE_KEY_PASSPHRASE));
+//        config.put("snowflake.role.name", confFromJson.get(Utils.SF_ROLE));
+        config.put("snowflake.role.name", "STREAMKAP_ROLE_JUNIT");
+//        config.put("snowflake.schema.name", confFromJson.get(Utils.SF_SCHEMA));
+        config.put(Utils.SF_SCHEMA, "JUNIT");
         config.put("snowflake.schematization.auto", "false");
         config.put("snowflake.topic2table.map",
                 "REGEX_MATCHER>^([-\\w]+\\.)([-\\w]+\\.)?([-\\w]+\\.)?([-\\w]+\\.)?([-\\w]+),$5");
-        config.put("snowflake.url.name", confFromJson.get(Utils.SF_URL));
-        config.put("snowflake.user.name", confFromJson.get(Utils.SF_USER));
+//        config.put("snowflake.url.name", confFromJson.get(Utils.SF_URL));
+
+        config.put(Utils.SF_URL, "sab25080.prod3.us-west-2.aws.snowflakecomputing.com");
+//        config.put("snowflake.user.name", confFromJson.get(Utils.SF_USER));
+
+        config.put(Utils.SF_USER, "STREAMKAP_USER_JUNIT");
         config.put("buffer.count.records", "0");
         config.put("buffer.flush.time", "-1");
 
@@ -76,7 +92,7 @@ public class StreamkapITSnowflake extends StreamkapSinkITBase<SnowflakeSinkTask>
 
     @Test
     public void testNominal() throws SQLException, InterruptedException {
-        super.testNominal();
+        super.testNominalInsert(false);
     }
 
     // @Test
@@ -112,10 +128,10 @@ public class StreamkapITSnowflake extends StreamkapSinkITBase<SnowflakeSinkTask>
         return SCHEMA_NAME;
     }
 
-    @Override
-    protected void configureIngestionMode(Map<String, String> config, boolean isUpsert) {
-        // config.put("databricks.ingestion.mode", isUpsert ? "upsert" : "append");
-    }
+//    @Override
+//    protected void configureIngestionMode(Map<String, String> config, boolean isUpsert) {
+//        // config.put("databricks.ingestion.mode", isUpsert ? "upsert" : "append");
+//    }
 
     @Override
     protected SinkRecord applyTransforms(SinkRecord record) {
