b921430377b2a350678737c6c3811bb17ba6cc3e
wrehman-skap <wasi@streamkap.com>
2025-12-05 16:17:44 +0000

    Updating jdbc partnerid string

--- src/main/java/com/snowflake/kafka/connector/internal/InternalUtils.java
+++ src/main/java/com/snowflake/kafka/connector/internal/InternalUtils.java
@@ -320,14 +320,14 @@ public class InternalUtils {
       String jdbcConfigMapInput = conf.get(SnowflakeSinkConnectorConfig.SNOWFLAKE_JDBC_MAP);
       Properties properties = new Properties();
       if (jdbcConfigMapInput == null) {
-        properties.put("application", "JDBC_Streamkap_Snowpipe");
+        properties.put("application", "JDBC_Streamkap_Snowpipe_Streaming");
         return properties;
       }
       Map<String, String> jdbcMap = Utils.parseCommaSeparatedKeyValuePairs(jdbcConfigMapInput);
       boolean hasApplicationKey = jdbcMap.keySet().stream()
               .anyMatch(k -> k.equalsIgnoreCase("application"));
       if (!hasApplicationKey) {
-          jdbcMap.put("application", "JDBC_Streamkap_Snowpipe");
+          jdbcMap.put("application", "JDBC_Streamkap_Snowpipe_Streaming");
       }
       properties.putAll(jdbcMap);
       return properties;
